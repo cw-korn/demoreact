f616c3424203cb21e8d6c715674d8362c7d14b01
PopKorn0505 <chawakorn.works@gmail.com>
2024-07-29 13:21:34 +0700

    first commit

---
+++ demo-react/src/App.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useState } from 'react'
+import './App.css'
+import InputField from "./Components/InputField";
+import { Todo } from "./model"
+
+// let name: string = "John";
+// let name: string;
+// let age: number|string;
+// let isStudent: boolean;
+// let hobbies: string[];
+// let role:[number,string];
+// let personName: unknown;
+// // let person:Object; // not recomended
+
+// interface Person  { //create object
+//   name: string,
+//   age?: number;
+// };
+
+// let person: Person = {
+//   name:"John",
+//   age:15
+// }
+
+// let manyPeople:Person[];
+
+// let printName:(x:string,y:number) => void = function(
+//   name:string,
+//   age: number
+// ):void{
+//   console.log("name is "+name+" age is "+ age)
+// }
+
+// printName("John",12);
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (todo){
+      setTodos([...todos,{id:Date.now(),todo ,isDone:false}])
+      setTodo("");
+    }
+  };
+  console.log(todos);
+  return (
+    <div className="App">
+      <span className="heading">Task Manager</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoList/> */}
+      {todos.map((t)=>(
+        <li>{t.todo}</li>
+      ))}
+    </div>
+  );
+};
+
+export default App
---
+++ demo-react/src/Components/cards.tsx
@@ -0,0 +1,10 @@
+function Card(props){
+    return(
+        <div className = "card">
+            <h1>{props.cardName}</h1>
+            <p>{props.description}</p>
+            <button className = "btn">Button</button>
+        </div>
+    );
+}
+export default Card;
